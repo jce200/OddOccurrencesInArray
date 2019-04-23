7545c18f86237e339766eda7194522b8871259f9
Jonathan <jchinsue@gmail.com>
2019-04-23 15:07:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 function solution(arr) {
     const createOccMap = (map, nextInteger) => {
         const isMissingKey = key => map[key] === undefined;
-
         if (isMissingKey(nextInteger)) {
             map[nextInteger] = 0;
         }
-
         ++map[nextInteger];
-
         return map;
     }
-
     const isOdd = number => (number % 2) !== 0;
     const IntOccMap = arr.reduce(createOccMap, {});
 
